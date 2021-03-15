89d7da585d2f816ac494084647f7d77d7bba2b6f
Daniel Madhala <danielmad87@gmail.com>
2021-03-15 16:59:08 +0200

    useState - basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello world'));
+  const [text, setText] = useState('random title');
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello daniel');
+    } else {
+      setText('random title');
+    }
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
