786d3bb55a1ae0d73e1dcb7388cd67c61aa149ec
Daniel Madhala <danielmad87@gmail.com>
2021-03-18 12:12:31 +0200

    useState - Functional Update Form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+
   const reset = () => setValue(0);
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => prevState + 1);
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -16,6 +24,14 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+
+        <button className="btn" onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
